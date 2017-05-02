64275bda455e2ab7115dbaa66901b901609cd5be
Ruy Guerra <ruy.cellphone@gmail.com>
2017-05-02 16:16:16 -0300

    BinaryGap

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "";
+            while (input != "exit")
+            {
+
+                input = Console.ReadLine();
+                Console.WriteLine(Solution.solution(int.Parse(input)));
+                //Console.WriteLine(">>>" + input + "<<<");
+
+            }
+
+
+        }
+    }
+}
---
+++ BinaryGap/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    class Solution
+    {
+
+
+
+        public static int solution(int N)
+        {
+            var maxGap = 0;
+            var count = 0;
+            var foundOne = false;
+
+            while ( N > 0)
+            {
+                if ((N & 1) == 0)
+                {
+                  count++;          
+                } else {
+                    if (foundOne && count > maxGap)
+                    {
+                        maxGap = count;
+                    }
+                    count = 0;
+                    foundOne = true;
+                }
+                N = N >> 1;
+            }
+
+            return maxGap;
+
+        }
+
+
+    }
+}
